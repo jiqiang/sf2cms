71102948ebea313b886bf1ab775b28a490fa46f8
jiqiang <qiang.ji@yahoo.com.au>
2015-04-19 23:00:58 +1000

    Generate user entity and CRUD.

---
+++ src/CustomCMSBundle/Form/UserType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CustomCMSBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password', 'password')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CustomCMSBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'customcmsbundle_user';
+    }
+}
