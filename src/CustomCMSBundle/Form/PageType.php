bad2f1813954cb64b0dd17c30122f5eb086a98c7
jiqiang <qiang.ji@yahoo.com.au>
2015-04-22 23:08:54 +1000

    Generate page entity, CRUD and relationships.

---
+++ src/CustomCMSBundle/Form/PageType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CustomCMSBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PageType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('category')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CustomCMSBundle\Entity\Page'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'customcmsbundle_page';
+    }
+}
